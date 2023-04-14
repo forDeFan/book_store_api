cd9b23f2b0f4265bcb0f0f57d08ece7b5d271776
tom <t-gutowski@o2.pl>
2023-04-14 20:33:55 +0200

    feat: add autentication endpoints to base urls

--- app/user/urls.py
+++ app/user/urls.py
@@ -2,9 +2,13 @@
 
 from django.urls import path
 from user import views
+from rest_framework_simplejwt.views import TokenRefreshView
 
 app_name = "user"
 
 urlpatterns = [
-    path("register/", views.CreateUser.as_view(), name="register")
+    path("register/", views.CreateUser.as_view(), name="register"),
+    path("login/", views.LoginView.as_view(), name="login"),
+    path("login/refresh/", TokenRefreshView.as_view(), name="refresh"), # Extend seesion time endpoint.
+    path("logout/", views.LogoutView.as_view(), name="logout"),
 ]
