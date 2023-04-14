69afcce3e0b08fc561e8b05878906aca4424d199
tom <t-gutowski@o2.pl>
2023-04-14 20:23:05 +0200

    feat: add JWT settings to be used in auth

--- app/bookstore/settings.py
+++ app/bookstore/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -47,10 +48,11 @@ INSTALLED_APPS = [
     # Project apps
     "bookstore",
     "core",
-    "user", # user api
+    "user",  # user api
     # Extra
     "django_probes",
     "rest_framework",
+    "rest_framework_simplejwt.token_blacklist",  # Logout from service
 ]
 
 MIDDLEWARE = [
@@ -141,3 +143,30 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Custom User model attached to Django
 AUTH_USER_MODEL = "core.User"
+
+# JWT settings
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "UPDATE_LAST_LOGIN": False,
+    "ALGORITHM": "HS256",
+    "SIGNING_KEY": SECRET_KEY,
+    "VERIFYING_KEY": None,
+    "AUDIENCE": None,
+    "ISSUER": None,
+    "AUTH_HEADER_TYPES": ("Bearer",),
+    "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
+    "USER_ID_FIELD": "id",
+    "USER_ID_CLAIM": "id",
+    "AUTH_TOKEN_CLASSES": (
+        "rest_framework_simplejwt.tokens.AccessToken",
+    ),
+    "TOKEN_TYPE_CLAIM": "token_type",
+    "JTI_CLAIM": "jti",
+    "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",
+    "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
+    "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
+}
