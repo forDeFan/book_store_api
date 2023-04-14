aaf24d6e3ecc9debabc02de3db79fec3c7da828e
tom <t-gutowski@o2.pl>
2023-04-14 20:33:19 +0200

    feat: add auth
    Add JWT as means of authentication for user login.

--- app/user/seralizers.py
+++ app/user/seralizers.py
@@ -1,8 +1,16 @@
-from typing import OrderedDict
+from typing import Dict, List, OrderedDict
 
 from core.models import User
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import (
+    TokenObtainPairSerializer,
+)
+from rest_framework_simplejwt.tokens import (
+    RefreshToken,
+    Token,
+    TokenError,
+)
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -11,13 +19,62 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         """Model and additional args to be passed to serializer (extra_kwargs)."""
 
-        model = get_user_model()
-        fields = ["email", "password", "name"]
+        model: User = get_user_model()
+        fields: List[str] = ["email", "password", "name"]
         # Prevent to return password in Response.
-        extra_kwargs = {
+        extra_kwargs: Dict[str, str] = {
             "password": {"write_only": True, "min_length": 5}
         }
 
     def create(self, validated_data: OrderedDict[str, str]) -> User:
         """Creates and return user."""
         return get_user_model().objects.create_user(**validated_data)
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    """
+    JWT token serializer.
+    Returns encoded access and refresh token.
+    """
+
+    @classmethod
+    def get_token(cls, user: User) -> Token:
+        """
+        Add custom fields and return encoded token.
+
+        Args:
+            user (User): for which token will be returned - if exists
+        Returns:
+            str: encoded token with custom fields
+        """
+        token: Token = super(
+            MyTokenObtainPairSerializer, cls
+        ).get_token(user)
+
+        return token
+
+
+class RefreshTokenSerializer(serializers.Serializer):
+    """
+    JWT refresh token serializer.
+    Used to blacklist refresh token.
+    """
+
+    refresh = serializers.CharField()
+    print(type(refresh))
+
+    default_error_messages: Dict[str, str] = {
+        "bad_token": ("Token is invalid or expired")
+    }
+
+    def validate(self, attrs: OrderedDict[str, str]) -> str:
+        """Get refresh token from request."""
+        self.token: str = attrs["refresh"]
+        return attrs
+
+    def save(self, **kwargs):
+        """Blacklist refresh token."""
+        try:
+            RefreshToken(self.token).blacklist()
+        except TokenError:
+            self.fail("bad_token")
--- app/user/views.py
+++ app/user/views.py
@@ -1,5 +1,15 @@
-from user.seralizers import UserSerializer
-from rest_framework import generics
+from rest_framework import generics, permissions, status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework_simplejwt.serializers import (
+    TokenObtainPairSerializer,
+)
+from rest_framework_simplejwt.views import TokenObtainPairView
+from user.seralizers import (
+    MyTokenObtainPairSerializer,
+    RefreshTokenSerializer,
+    UserSerializer,
+)
 
 
 class CreateUser(generics.CreateAPIView):
@@ -9,4 +19,30 @@ class CreateUser(generics.CreateAPIView):
     (check if exist by email i.e.)
     """
 
-    serializer_class = UserSerializer
+    serializer_class: UserSerializer = UserSerializer
+
+
+class LoginView(TokenObtainPairView):
+    """
+    API view to log in (obtain access and refresh token).
+    """
+
+    permission_classes: permissions = (AllowAny,)
+    serializer_class: TokenObtainPairSerializer = (
+        MyTokenObtainPairSerializer
+    )
+
+
+class LogoutView(generics.GenericAPIView):
+    """
+    API view to log out (refresh token blacklist).
+    Access token will expire on setting defined time period itself.
+    """
+    serializer_class = RefreshTokenSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, *args):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
