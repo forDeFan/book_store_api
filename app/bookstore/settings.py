5c38b303c66b0c85a410cb3d815a1a507c57a7d5
tom <t-gutowski@o2.pl>
2023-04-16 12:14:43 +0200

    fix: authentication lib
    Add jwt authentication on endpoints by pinning simple_jwt lib to rest_framework in settings.

--- app/bookstore/settings.py
+++ app/bookstore/settings.py
@@ -177,6 +177,9 @@ SIMPLE_JWT = {
 # Rest framework settings
 
 REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
     "DEFAULT_FILTER_BACKENDS": (
         "django_filters.rest_framework.DjangoFilterBackend",
     ),
