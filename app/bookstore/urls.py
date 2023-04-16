e6d818544e9c6f22668b6eeecd19d58d3dc41d68
tom <t-gutowski@o2.pl>
2023-04-16 13:19:07 +0200

    feat: add images upload to images folder

--- app/bookstore/urls.py
+++ app/bookstore/urls.py
@@ -3,10 +3,15 @@
 from django.contrib import admin
 from django.urls import include, path
 
+ 
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     # Include endpoints from user api
     path("api/user/", include("user.urls")),
     # Include endpoints from book api
     path("api/book/", include("book.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # Images
