90de272e709903f536886473aaeca75e3f23f229
tom <t-gutowski@o2.pl>
2023-04-16 20:27:24 +0200

    chore: attached default django start site for better user exp

--- app/bookstore/urls.py
+++ app/bookstore/urls.py
@@ -1,13 +1,14 @@
 """Central file for url mappings/ menaging."""
 
-from django.contrib import admin
-from django.urls import include, path
-
-
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from django.views import debug
 
 urlpatterns = [
+    # Default django site - just to see something at app start.
+    path("", debug.default_urlconf),
     path("admin/", admin.site.urls),
     # Include endpoints from user api
     path("api/user/", include("user.urls")),
